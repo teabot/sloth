93f6454886dec602bbe4744ca67751085e2af9d6
Elliot West <teabot@gmail.com>
2013-01-25 11:58:37 +0000

    Made this into a daemon

--- sloth.py
+++ sloth.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
-import sys
+import sys, signal
 from jenkins import *
 from ledstrip import *
 
+def signal_handler(signal, frame):
+  shutdown()
+
+def shutdown():
+  ledStrip.switch_off()
+  sys.exit(0)
+
 def main():
+  signal.signal(signal.SIGTERM, signal_handler)
+
   jenkins = JenkinsBuildStatus("http://hudson2.datadev.last.fm:8080/")
   jenkins.start()
   try:
@@ -12,8 +21,7 @@ def main():
       delay = jenkins.update(ledStrip)
       time.sleep(delay)
   except KeyboardInterrupt:
-    ledStrip.switch_off()
-    sys.exit(0) 
+    shutdown()
 
 ledStrip = LedStrip(32, "/dev/spidev0.0")
 
