6a9a5496759f0ab02ad02397653c4be444f2f7c9
Elliot West <teabot@gmail.com>
2013-01-28 12:04:11 +0000

    Colour cycling

--- lunch.py
+++ lunch.py
@@ -15,8 +15,11 @@ class Lunch():
   def update(self, ledStrip):
     colours = list()
     for i in range(ledStrip.stripLength):
-      hue = self.hueOffset + (self.hueIncrement * i) 
+      hue = (self.hueOffset + (self.hueIncrement * i)) % 1.0
       colours.append(newHsvColour(hue))
     ledStrip.push_colours(colours)
-    return 1.0
+    self.hueOffset += self.hueOffsetIncrement
+    if self.hueOffset > 1.0:
+      self.hueOffset = 0.0
+    return 0.01 
 
